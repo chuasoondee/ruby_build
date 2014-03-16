7d9252478704d026df72238b769daa356f35062f
Chua Soon Dee <soondee.chua@gmail.com>
2014-03-16 23:11:00 +0800

    use home dir to build instead of vagrant mounted volume

--- package.sh
+++ package.sh
@@ -5,17 +5,18 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Install build dependencies
 sudo yum groupinstall -y 'Development Tools' --skip-broken
+sudo yum install -y openssl-devel zlib-devel gcc gcc-c++ autoconf readline-devel curl-devel expat-devel gettext-devel patch readline zlib libyaml-devel libffi-devel make bzip2
 sudo yum install -y ruby rubygems ruby-devel libyaml-devel # install ruby-libs as dependency
 sudo gem install fpm
 
 # Download ruby source
 curl -O http://cache.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p484.tar.gz
 
-tar xf ruby-1.9.3-p484.tar.gz
+tar xf ruby-1.9.3-p484.tar.gz -C $HOME
 
 mkdir -p /tmp/installdir
 
-pushd $DIR/ruby-1.9.3-p484
+pushd $HOME/ruby-1.9.3-p484
 ./configure --prefix=/usr && make install DESTDIR=/tmp/installdir
 popd
 
